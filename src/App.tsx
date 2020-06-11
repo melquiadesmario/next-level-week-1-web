5b757b8e765f554406828a859a545eadcf9a43e8
melquiadesmario <melquiadesm@gmail.com>
2020-06-10 22:25:09 -0300

    Concepts Components, Props and State

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './App.css'
 
+import Header from './Header'
+
 function App() {
+  const [counter, setCounter] = useState(0)
+
+  function handleButtonClick(){
+    setCounter(counter + 1)
+  }
+
   return (
-      <h1>Hello Dev</h1>
+      <div>
+        <Header title={ `Counter: ${ counter }` } />
+        <h1>{ counter } * 2 = { counter * 2 }</h1>
+        <h1>{ counter } / 2 = { counter / 2 }</h1>
+        <button type='submit' onClick={ handleButtonClick }>Increment</button>
+      </div>
   )
 }
 
