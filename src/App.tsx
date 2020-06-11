a7f17dbdf55055a6f79b2dd5960d87801ba51716
melquiadesmario <melquiadesm@gmail.com>
2020-06-10 22:36:38 -0300

    Global Styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,11 @@
 import React, { useState } from 'react'
 import './App.css'
 
-import Header from './Header'
-
 function App() {
-  const [counter, setCounter] = useState(0)
-
-  function handleButtonClick(){
-    setCounter(counter + 1)
-  }
 
   return (
       <div>
-        <Header title={ `Counter: ${ counter }` } />
-        <h1>{ counter } * 2 = { counter * 2 }</h1>
-        <h1>{ counter } / 2 = { counter / 2 }</h1>
-        <button type='submit' onClick={ handleButtonClick }>Increment</button>
+        <h1>Hello Dev</h1>
       </div>
   )
 }
